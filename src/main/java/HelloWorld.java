831bfac20480a335f9a6caeb1f5d97096373bd0c
bmwu <bmwu.hz@gmail.com>
2017-03-05 23:09:09 +0800

    hello world和PrimitiveType

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by michael on 3/5/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] argcs) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/PrimitiveType.java
@@ -0,0 +1,37 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
+/**
+ * Created by michael on 3/5/17.
+ */
+public class PrimitiveType {
+
+    byte b = Byte.SIZE;
+    byte bMax = Byte.MAX_VALUE;
+    byte bMin = Byte.MIN_VALUE;
+
+    short aShort = Short.SIZE;
+    short aShortMax = Short.MAX_VALUE;
+    short aShortMin = Short.MIN_VALUE;
+
+    int anInt = Integer.SIZE;
+    int anIntMax = Integer.MAX_VALUE;
+    int anIntMin = Integer.MIN_VALUE;
+
+    long aLong = Long.SIZE;
+    long aLongMax = Long.MAX_VALUE;
+    long aLongMin = Long.MIN_VALUE;
+
+    float aFloat = Float.SIZE;
+    float aFloatMax = Float.MAX_VALUE;
+    float aFloatMin = Float.MIN_VALUE;
+
+    double aDouble = Double.SIZE;
+    double aDoubleMax = Double.MAX_VALUE;
+    double aDoubleMin = Double.MIN_VALUE;
+
+    boolean aBoolean = true;
+
+    char aChar = 'A';
+    char aCharNum = 100;
+
+}
