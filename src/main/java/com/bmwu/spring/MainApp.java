99cdb39a9c6ceb09f1a33e434cbc031dc320c2b1
bmwu <bmwu.hz@gmail.com>
2017-03-28 23:47:45 +0800

    spring - bean - scope - 增加hashcode 更加直观的比较

--- src/main/java/com/bmwu/spring/MainApp.java
+++ src/main/java/com/bmwu/spring/MainApp.java
@@ -17,27 +17,33 @@ public class MainApp {
 
         HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
         helloWorld.getMessage();
+        System.out.println("helloWorld hashcode: " + helloWorld.hashCode());
 
         // @Deprecated
         XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
         HelloWorld helloWorld1 = (HelloWorld) xmlBeanFactory.getBean("helloWorld");
         helloWorld1.getMessage();
+        System.out.println("helloWorld1 hashcode: " + helloWorld1.hashCode());
 
         // scope = singleton
         HelloWorld helloWorld2 = (HelloWorld) context.getBean("helloWorldScope");
         helloWorld2.setMessage("scope");
         helloWorld2.getMessage();
+        System.out.println("helloWorld2 hashcode: " + helloWorld2.hashCode());
 
         HelloWorld helloWorld3 = (HelloWorld) context.getBean("helloWorldScope");
         helloWorld3.getMessage();
+        System.out.println("helloWorld3 hashcode: " + helloWorld3.hashCode());
 
         // scope = prototype
         HelloWorld helloWorld4 = (HelloWorld) context.getBean("helloWorldScope2");
         helloWorld4.setMessage("scope");
         helloWorld4.getMessage();
+        System.out.println("helloWorld4 hashcode: " + helloWorld4.hashCode());
 
         HelloWorld helloWorld5 = (HelloWorld) context.getBean("helloWorldScope2");
         helloWorld5.getMessage();
+        System.out.println("helloWorld5 hashcode: " + helloWorld5.hashCode());
 
     }
 }
