2f28544e827c983e655ae918fc3bb91f069c89c3
bmwu <bmwu.hz@gmail.com>
2020-02-13 21:31:10 +0800

    update

--- src/main/java/com/bmwu/HelloWorld.java
+++ src/main/java/com/bmwu/HelloWorld.java
@@ -1,5 +1,8 @@
 package com.bmwu;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by michael on 3/5/17.
  */
@@ -10,7 +13,14 @@ public class HelloWorld {
     }
 
     public static void main(String[] args) {
-        Double ratio1 = getRatio();
-        Double ratio2 = 1.0 - ratio1;
+        Map<String, String> map = new HashMap<>();
+        map.put("key", null);
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+//        Double ratio1 = getRatio();
+//        Double ratio2 = 1.0 - ratio1;
     }
 }
--- src/main/java/com/bmwu/utils/javaSDK.java
+++ src/main/java/com/bmwu/utils/javaSDK.java
@@ -1,21 +1,21 @@
 package com.bmwu.utils;
-
-import com.bmwu.javasdk.*;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-/**
- * Created by michael on 9/28/17.
- */
+//
+//import com.bmwu.javasdk.*;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertEquals;
+//
+///**
+// * Created by michael on 9/28/17.
+// */
 public class javaSDK {
-
-    @Test
-    public void javaSDKTest() {
-        sdk s= new sdk();
-        String rawData = "1233";
-        String cryptData = s.encrypt(rawData);
-        String result = s.decrypt(cryptData);
-        assertEquals(rawData, result);
-    }
+//
+//    @Test
+//    public void javaSDKTest() {
+//        sdk s= new sdk();
+//        String rawData = "1233";
+//        String cryptData = s.encrypt(rawData);
+//        String result = s.decrypt(cryptData);
+//        assertEquals(rawData, result);
+//    }
 }
